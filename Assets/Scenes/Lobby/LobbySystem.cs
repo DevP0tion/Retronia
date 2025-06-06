edb836a237e3735bc6febbfe95cc7049717e0854
AmteurPotion <1k0j2i4@gmail.com>
2025-06-07 00:56:55 +0900

    [Feat] 기초 UI 구현
    * Menu, Status UI 구현
    * Menu UI 로직 구현

--- Assets/Scenes/Lobby/LobbySystem.cs
+++ Assets/Scenes/Lobby/LobbySystem.cs
@@ -1,35 +1,45 @@
 using Retronia.IO;
-using Retronia.Utils;
+using Retronia.Utils.UI;
 using TMPro;
 using UnityEngine;
+
+using CharacterInfo = Retronia.IO.Formats.CharacterInfo;
 
 namespace Retronia.Scenes.Lobby
 {
   public class LobbySystem : MonoBehaviour
   {
     private static SAVE Data => SAVE.Current;
+    private static CharacterInfo PlayerData => CharacterInfo.Current;
     
     #region Menu UI
-    [Header( "Menu UI" )]
+    [Header( "Menu UI / Binding" )]
     
     [SerializeField] private GameObject menuUI;
-    [SerializeField] private TMP_Text elementalText, nameText, levelText;
+    [SerializeField] private TMP_Text elementalText, nameText, levelText, expText, goldText;
+    [SerializeField] private UIGaugeBar expBar;
 
     private void InitMenu()
     {
-      if (Data != null)
+      if (PlayerData != null)
       {
-        elementalText.text = Data.GetString("elemental", "normal");
-        nameText.text = Data.name;
-        levelText.text = Data.name;
+        elementalText.text = "속성 - " + PlayerData.elemental;
+        nameText.text = PlayerData.name;
+        levelText.text = PlayerData.level.ToString();
+        goldText.text = $"{PlayerData.gold:#,###}";
+        
+        expBar.max = PlayerData.MaxExp;
+        expBar.Value = PlayerData.exp;
+        expText.text = PlayerData.exp + " / " + PlayerData.MaxExp;
       }
     }
     #endregion
     
     #region Status UI
-    [Header( "Status UI" )]
+    [Header( "Status UI / Binding" )]
     
     [SerializeField] private GameObject statusUI;
+    [SerializeField] private TMP_Text atkText, defText, hpText, critChanceText;
     
     private void InitStatus()
     {
