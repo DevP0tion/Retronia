0671d7c713f491dc41b344795d98a0f9fd2ec8b3
AmteurPotion <1k0j2i4@gmail.com>
2025-06-07 22:32:38 +0900

    [Fix] 널 참조 오류 수정

--- Assets/Scenes/MainMenu/MenuSystem.cs
+++ Assets/Scenes/MainMenu/MenuSystem.cs
@@ -23,6 +23,7 @@ namespace Retronia.Scenes.MainMenu
     {
       // TODO 생성시 명칭 설정 가능하게 구현
       SAVE.Current = new SAVE(nameLabel.text);
+      SAVE.Current.Init();
       SceneManager.LoadScene(SceneNames.Lobby);
     }
     
