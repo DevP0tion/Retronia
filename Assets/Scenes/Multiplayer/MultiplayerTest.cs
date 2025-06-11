4475dc579ccb8faf7162ebe99453ab22dd466cc9
AmteurPotion <1k0j2i4@gmail.com>
2025-06-12 02:24:58 +0900

    [Test] 노트북 유니티 에디터(와이파이) - 안드로이드 클라이언트(데이터) 간 멀티플레이어 테스트 성공

--- Assets/Scenes/Multiplayer/MultiplayerTest.cs
+++ Assets/Scenes/Multiplayer/MultiplayerTest.cs
@@ -1,22 +1,24 @@
 using Retronia.Networking;
+using TMPro;
 using UnityEngine;
 
 namespace Retronia.Scenes.Multiplayer
 {
   public class MultiplayerTest : MonoBehaviour
   {
-    public void CreateRoom()
+    public void CreateRoom(TMP_InputField field)
     {
       var manager = RoomManager.singleton;
+      manager.networkAddress = field.text;
       
       manager.StartHost();
     }
 
-    public void JoinRoom()
+    public void JoinRoom(TMP_InputField field)
     {
       var manager = RoomManager.singleton;
-      
-      
+      manager.networkAddress = field.text;
+
       
       manager.StartClient();
     }
